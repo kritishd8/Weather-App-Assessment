7f054e092f23311136e5abd423d150eeeab263f0
kritishd8 <dhakalkritish7@gmail.com>
2024-01-23 11:02:44 +0545

    caching done

--- script.js
+++ script.js
@@ -11,22 +11,20 @@ let searchBar = document.querySelector(".search-bar");
 let searchBtn = document.querySelector(".search button");
 
 
-// Creating a object literal
+//? Creating a object literal
 let weather = {
   //? getting the weather data
   fetchWeather: function (city) {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data))
-      .catch((err) => this.notFound())
+    fetch(`weather.php?city=${city}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+        .catch(() => this.notFound());
   },
 
   //? function to display the weather data in html using dom
   displayWeather: function (data) {
 
-    const date = new Date(data.dt * 1000);
+    const date = new Date(data.last_updated);
     //? Converting the date to "Dec 29, 2023" format
     const dateTime = date.toLocaleDateString(
       "en-US", 
@@ -39,17 +37,18 @@ let weather = {
     document.querySelector('.weather').style.display = 'block';
     document.querySelector('.not-found').style.display = 'none';  
 
-    document.querySelector(".city").innerHTML = `${data.name}, ${data.sys.country}`;
-    document.querySelector(".description").innerHTML = data.weather[0].description;
-    document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`; //? Putting the description suitable icon //
-    document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
-    document.querySelector(".humidity").innerHTML = `<b>Humidity</b>: ${data.main.humidity}%`;
-    document.querySelector(".pressure").innerHTML = `<b>Pressure</b>: ${data.main.pressure}Pa`;
-    document.querySelector(".wind").innerHTML = `<b>Wind Speed</b>: ${data.wind.speed}km/h`;
+    console.log(data);
+    document.querySelector(".city").innerHTML = `${data.city}, ${data.country}`;
+    document.querySelector(".description").innerHTML = data.weather_description;
+    document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${data.icon}.png`; //? Putting the description suitable icon //
+    document.querySelector(".temp").innerHTML = `${Math.round(data.temperature)}°C`;
+    document.querySelector(".humidity").innerHTML = `<b>Humidity</b>: ${data.humidity}%`;
+    document.querySelector(".pressure").innerHTML = `<b>Pressure</b>: ${data.pressure}Pa`;
+    document.querySelector(".wind").innerHTML = `<b>Wind Speed</b>: ${data.wind_speed}km/h`;
     document.querySelector(".dateTime").innerHTML = `${dateTime}`;
 
     //? Getting a image of the city from unsplash link
-    document.body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${data.name}')`;
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${data.city}')`;
   },
 
   //? Search function
@@ -64,7 +63,6 @@ let weather = {
     document.querySelector('.not-found').style.display = 'block';
     document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1650692201357-3b1b15469952?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`;
   },
-
 };
 
 //? Search when btn is clicked
